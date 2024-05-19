92d8b3da7f84d9a413680d0804787e203de3eeee
aykhan <Aykhan.Alifov@azai.tech>
2024-05-19 18:26:18 +0400

    Add search

--- src/components/MainOrderView/Menu.vue
+++ src/components/MainOrderView/Menu.vue
@@ -2,7 +2,7 @@
     <div class="menu">
         <div class="menu-category-item-menu sticky">
             <div class="menu-category-items-header">
-                <span :class="{ active: selectedCategory === undefind }" @click="fetchMenuItems()">
+                <span :class="{ active: selectedCategory === undefined }" @click="fetchMenuItems()">
                     Əsas yeməklər
                 </span> 
                 <span v-for="category in mealCategories" :key="category.id" @click="fetchMenuItems(category.id)"
@@ -10,10 +10,10 @@
                     {{ category.name }}
                 </span>
             </div>
+            <input type="text" v-model="searchQuery" placeholder="Axtarış..." class="search-input">
         </div>
-        <div class="menu-item" v-for="item in menuItems" :key="item.id" @click="addOrderItem(tableId, item.id, 1)"
+        <div class="menu-item" v-for="item in filteredMenuItems" :key="item.id" @click="addOrderItem(tableId, item.id, 1)"
             :class="{ 'disabled': loading }">
-            <!-- Display the menu item name here -->
             <div class="menu-item-name">{{ item.name }}</div>
             <div class="menu-item-price">{{ item.price }} azn</div>
         </div>
@@ -37,13 +37,23 @@ export default {
             mealCategories: [],
             menuItems: [],
             selectedCategory: null,
-            loading: false, // Add loading state,
+            loading: false,
             orderCreatedAlready: false,
+            searchQuery: '', // Add search query state
         };
     },
     async created() {
         this.fetchMealCategories();
         this.fetchMenuItems();
+    },
+    computed: {
+        filteredMenuItems() {
+            if (!this.searchQuery) {
+                return this.menuItems;
+            }
+            const query = this.searchQuery.toLowerCase();
+            return this.menuItems.filter(item => item.name.toLowerCase().includes(query));
+        }
     },
     methods: {
         async checkOrderIfCreated() {
@@ -51,10 +61,8 @@ export default {
                 await this.createOrder();
                 this.orderCreatedAlready = true;
             }
-        }
-        ,
+        },
         async createOrder() {
-            // Call the API to create an order
             try {
                 await backendServices.createOrder(this.tableId);
                 console.log('Order created successfully for table ID:', this.tableId);
@@ -62,14 +70,11 @@ export default {
                 if (error.response && error.response.status === 400) {
                     console.log(error.response);
                     console.error(`Bad Request: ${error.response.data.error}`);
-                    // Handle 400 Bad Request error
                 } else {
                     console.error('Error creating order:', error);
-                    // Handle other errors
                 }
             }
         },
-
         async fetchMealCategories() {
             try {
                 const categories = await backendServices.fetchMealCategories();
@@ -80,14 +85,14 @@ export default {
         },
         async fetchMenuItems(categoryId) {
             try {
-                this.loading = true; // Set loading to true
+                this.loading = true;
                 const items = await backendServices.fetchMealsByCategoryId(categoryId);
                 this.menuItems = items;
                 this.selectedCategory = categoryId;
             } catch (error) {
                 console.error('Error fetching menu items:', error);
             } finally {
-                this.loading = false; // Set loading back to false regardless of success or failure
+                this.loading = false;
                 if (!categoryId) {
                     document.querySelector('.active-green').classList.remove('active-green');
                 }
@@ -96,14 +101,13 @@ export default {
         async addOrderItem(categoryId, mealId, quantity) {
             try {
                 await this.checkOrderIfCreated();
-
-                this.loading = true; // Set loading to true
+                this.loading = true;
                 await backendServices.addOrderItem(categoryId, mealId, quantity);
                 EventBus.emit('orderItemAdded');
             } catch (error) {
                 console.error('Error while adding meal to order:', error);
             } finally {
-                this.loading = false; // Set loading back to false regardless of success or failure
+                this.loading = false;
             }
         }
     }
@@ -115,12 +119,9 @@ export default {
     max-height: 300px;
 }
 
-/* Add styling for disabled menu items */
 .disabled {
     pointer-events: none;
-    /* Disable click events */
     opacity: 0.5;
-    /* Optionally reduce opacity to visually indicate it's disabled */
 }
 
 .active {
@@ -129,7 +130,7 @@ export default {
     transition: all 0.3ms ease-in-out;
 }
 
-.active:hover{
+.active:hover {
     background-color: rgb(145, 223, 27);
 }
 
@@ -137,25 +138,18 @@ export default {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(15, 1fr);
-    /* Creates three columns */
     gap: 10px;
     padding: 0 5px;
     overflow-y: auto;
-    /* Enable vertical scrolling */
-
-
 }
 
 .menu-item {
-    /* Styling for each menu item, adjust as needed */
     border: 2px solid black;
     padding: 5px;
     cursor: pointer;
     display: flex;
-    align-items:center;
-    /* This centers the item name vertically */
+    align-items: center;
     justify-content: space-between;
-    /* This centers the item name horizontally */
     height: 60px;
     max-width: 200px;
     background-color: cadetblue;
@@ -163,17 +157,16 @@ export default {
     overflow-wrap: break-word;
 }
 .menu-item-name,
-.menu-item-price{
-    font-size: 14px
+.menu-item-price {
+    font-size: 18px;
+    font-weight: bold;
 }
 
 .menu-category-item-menu.sticky {
     grid-column: 1 / -1;
-    /* This makes it span all columns */
     position: sticky;
-    bottom: 0;
+    top: 0;
     background: white;
-    /* Optional: Background color for visibility */
     z-index: 1000;
 }
 
@@ -182,19 +175,22 @@ export default {
     gap: 5px;
     flex-wrap: wrap;
     white-space: nowrap;
-    cursor:pointer;
+    cursor: pointer;
     background-color: cadetblue;
     border-left: 4px solid cadetblue;
     border-right: 4px solid cadetblue;
 }
+
 .menu-category-items-header span:not(.active) {
-  background-color: #fff; 
+    background-color: #fff;
 }
 
 .menu-category-items-header::-webkit-scrollbar {
     display: none;
 } 
 .menu-category-items-header span {
+    font-size: 18px;
+    font-weight: bold;
     border-left: 2px solid cadetblue;
     border-right: 2px solid cadetblue;
     border-top: 6px solid cadetblue;   
@@ -202,31 +198,28 @@ export default {
     padding: 4px 10px;
 }
 
-.menu-category-item-menu.sticky {
-    grid-column: 1 / -1;
-    /* Makes it span all columns */
-    position: sticky;
-    top: 0;
-    /* Changed from bottom to top for better visibility */
-    background: white;
-    z-index: 1000;
+.search-input {
+    margin-top: 10px;
+    padding: 5px;
+    width: 100%;
+    min-width: 40px;
+    min-height: 60px;
+    box-sizing: border-box;
 }
+
 @media (max-width: 1034px) {
   .menu {
     grid-template-columns: repeat(2, 1fr);
   }
- 
 }
 
 @media (max-width: 768px) {
   .menu {
-    grid-template-columns: repeat(3, 1fr); 
-    }
-  .menu-category-items-header{
-    overflow-x: auto;
-    flex-wrap: nowrap;
+    grid-template-columns: repeat(2, 1fr); /* 2 columns */
+    grid-template-rows: repeat(2, 1fr); /* 2 rows */
   }
 }
+
 @media (max-width: 530px) {
   .menu {
     grid-template-columns: repeat(2, 1fr);
