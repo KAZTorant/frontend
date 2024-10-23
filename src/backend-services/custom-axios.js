92b49e00a795559e38ffc2247fc18aed9a54bc2b
Zakir Aghayev <zakir.aghayev.19@gmail.com>
2024-10-24 00:25:02 +0400

    WINDOW

--- src/backend-services/custom-axios.js
+++ src/backend-services/custom-axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL, // Set your base URL
+  baseURL: `${window.location.protocol}//${window.location.hostname}:8000`, // Set your base URL
   timeout: 5000, // Set a timeout
   headers: {
     'Content-Type': 'application/json',
